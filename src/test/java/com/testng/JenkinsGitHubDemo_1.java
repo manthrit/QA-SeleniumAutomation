139b4c532904f96eb611e9ea8f8b5fd8f7d211b8
manthrit <manthri.t@gmail.com>
2021-01-04 22:36:05 -0800

    JenkinsGitHub Demo Test and TestNG files

--- src/test/java/com/testng/JenkinsGitHubDemo_1.java
+++ src/test/java/com/testng/JenkinsGitHubDemo_1.java
@@ -2,6 +2,7 @@ package com.testng;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Parameters;
@@ -11,19 +12,28 @@ public class JenkinsGitHubDemo_1
 {
 
 	@Test
+	//@Parameters("browserType")
 	public void parameterizedTest_01(String browserType){
 		WebDriver driver;
 		switch(browserType)
 		{
 			case "firefox":
+					System.setProperty("webdriver.gecko.driver", "D://Software//BrowserDrivers//geckodriver.exe");
 					driver = new FirefoxDriver();
+					driver.get("https://www.google.com/");
 					break;
 			case "chrome":
+					System.setProperty("webdriver.chrome.driver", "D://Software//BrowserDrivers//chromedriver.exe");
 					driver = new ChromeDriver();
+					driver.get("https://www.google.com/");
 					break;
 			case "IE":
 					driver = new InternetExplorerDriver();
 					break;
+			case "edge":
+					System.setProperty("webdriver.edge.driver", "D://Software//BrowserDrivers//msedgedriver.exe");
+					driver=new EdgeDriver();
+					driver.get("https://www.google.com/");
 			default:
 					System.out.println("browser : " + browserType + " is invalid, Launching Firefox as browser of choice..");
 					driver = new ChromeDriver();
